ae58e0d55bdcd21054f9f9a3ba6947a40979838c
Xau <frozzy.xfs@gmail.com>
2023-09-05 01:57:00 -0400

    Add mergeSort

---
+++ mergeSort.js
@@ -0,0 +1,35 @@
+function merge(arr1, arr2) {
+    let newArr = [];
+    const newArr1 = [...arr1];
+    const newArr2 = [...arr2];
+    while (newArr1.length > 0 && newArr2.length > 0) {
+        if (newArr1[0] < newArr2[0]){
+            newArr = [...newArr, newArr1[0]];
+            newArr1.splice(0, 1);
+        }
+        else{
+            newArr = [...newArr, newArr2[0]];
+            newArr2.splice(0, 1);
+        }
+    }
+
+    // arr + remaining element in either array
+    return [...newArr, ...newArr1, ...newArr2];
+}
+
+function mergeSort(arr) {
+    if (arr.length <= 1) return arr;
+    else {
+        const mid = arr.length / 2;
+        const arr1 = mergeSort(arr.slice(0, mid));
+        const arr2 = mergeSort(arr.slice(mid));
+
+        return merge(arr1, arr2);
+    }
+}
+
+const arr1 = [3,2,1,4];
+console.log(arr1, '->', mergeSort(arr1));
+
+const arr2 = [4,2,6,7,1,5,3,8,9,0];
+console.log(arr2, '->', mergeSort(arr2));
